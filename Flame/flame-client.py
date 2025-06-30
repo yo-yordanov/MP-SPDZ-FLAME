a1e60a45bf5a7e69e904a5766016e411187abaac
Yordan Yordanov <y_yordanov@outlook.com>
2025-06-30 23:16:54 +0200

    Implement norm and cosine similarity calculation

--- Flame/flame-client.py
+++ Flame/flame-client.py
@@ -8,6 +8,8 @@ sys.path.append('.')
 from client import *
 from domains import *
 
+import numpy as np
+
 PRECISION = 32
 
 client_id = int(sys.argv[1])
@@ -27,7 +29,10 @@ def run(n, p):
     Generate and send random model update with n weights and precision p to all clients.
     """
     model = [random.uniform(-1, 1) for _ in range(model_size)]
-    # print(f"Client {client_id} sending model: {model}")
+
+    # norm = np.linalg.norm(model)
+    # print(f"Client {client_id} sending model: {model} with norm {norm}")
+
     model = [int(x * (1 << p)) for x in model]  # Convert to int representation
     client.send_private_inputs(model)
 
